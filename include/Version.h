6680b0f6b0a34af4a8302f187d7cc6c2d28b9c7a
ArminPP <app@gmx.at>
2025-01-25 18:12:34 +0100

    added WDT, some minor changes

--- include/Version.h
+++ include/Version.h
@@ -1,11 +1,11 @@
 
 #define Version "USS V0" // Armins Version: is static for the project! 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "50"
+  #define BUILD_NUMBER "72"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.5.241230_50 - 2024-12-30 12:58:06.898766"
+  #define VERSION "v0.5.250125_72 - 2025-01-25 17:30:18.773480"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.5.241230_50"
+  #define VERSION_SHORT "v0.5.250125_72"
 #endif
